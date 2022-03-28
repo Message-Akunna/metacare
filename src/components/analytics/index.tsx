0922bc6114b954e24259c94511457aaa3f8850b4
Message Akunna <message.ao@gmail.com>
2022-03-28 12:30:01 +0100

    sidebar design completed

---
+++ src/components/analytics/index.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+// 
+import Button from 'react-bootstrap/Button';
+
+
+const Analytics = () => {
+    return (
+        <Fragment>
+            <Button variant="primary" size="lg" className=''>Testing sass</Button>
+        </Fragment>
+    )
+}
+
+export default Analytics;
